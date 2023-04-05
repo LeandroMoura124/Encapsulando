b5f3945b696167be5674df09b7a560d95fdb9d9f
Leandro.Moura124 <leofcb334@gmail.com>
2023-04-05 16:04:28 -0300

    commit Encapsulamento

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    //método main
+    public static void main(String[] args) throws Exception {
+        ControleRemoto c1 = new ControleRemoto();
+        c1.ligar();
+        c1.aumentarVolume();
+        c1.diminuirVolume();
+        c1.pause();
+        c1.ligarMudo();
+        c1.desligar();
+       
+        c1.abrirMenu();
+    }
+}
---
+++ src/ControleRemoto.java
@@ -0,0 +1,115 @@
+public class ControleRemoto implements Controlador{
+    //Atributos
+    private int volume; 
+    private boolean ligado;
+    private boolean tocando;
+
+    //Método construtor
+    public ControleRemoto(){ // quando eu instancio o meu objeto, ele começa com esses valores de entrada
+        this.SetVolume(50);
+        this.SetLigado(true);
+        this.SetTocando(false);
+    }
+    //Métodos especiais
+    private void SetVolume(int v){ // posso sim trabalha com metodo get e sett privado - mas quando for manuser ele no objeto nao podemos usar set nem get - vamos puxar o nome do método e inserir valores
+        this.volume = v;
+    }
+    private int GetVolume(){
+        return this.volume;
+    }
+    private void SetLigado(boolean l){
+        this.ligado = l;
+    }
+    private boolean GetLigado(){
+        return this.ligado;
+    }
+    private void SetTocando(boolean t){
+        this.tocando = t;
+    }
+    private boolean GetTocando(){
+        return this.tocando;
+    }
+    @Override
+    public void ligar() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'ligar'");
+        this.SetLigado(true);
+    }
+    @Override
+    public void desligar() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'desligar'");
+        this.SetLigado(false);
+    }
+    @Override
+    public void abrirMenu() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'abrirMenu'");
+        System.out.println("Está ligado?: " + this.GetLigado());
+        System.out.println("Está tocando?: " + this.GetTocando());
+        System.out.println("Volume: " + this.GetVolume());
+        for(int i=0; i<this.GetVolume(); i+=10){
+            System.out.print("[]");
+        }
+
+    }
+    @Override
+    public void fecharMenu() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'fecharMenu'");
+        System.out.print("Fechando menu....");
+    }
+    @Override
+    public void aumentarVolume() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'aumentarVolume'");
+        if (this.GetLigado()){
+            this.SetVolume(this.GetVolume()+1);
+          System.out.println("O volume aumentou para: " + this.GetVolume());
+        
+        }
+    }
+    @Override
+    public void diminuirVolume() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'diminuirVolume'");
+        if (this.GetLigado()){
+            this.SetVolume(this.GetVolume()-1);
+            System.out.println("O volume diminuiu para: " + this.GetVolume());
+        }
+        
+    }
+    @Override
+    public void ligarMudo() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'ligarMudo'");
+        if(this.GetLigado() && this.GetVolume() > 0){
+            this.SetVolume(0);
+        }
+    }
+    @Override
+    public void desligarMudo(){
+        if(this.GetLigado() && this.GetVolume() == 0){
+            this.SetVolume(50);
+        }
+    }
+    @Override
+    public void play() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'play'");
+        if(this.GetLigado() && !(this.GetTocando())){
+            this.SetTocando(true);
+        }
+    }
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'pause'");
+        if(this.GetLigado() && this.GetTocando()){
+            this.SetTocando(false);
+        }
+    }
+    
+
+    //método especifícos
+}
